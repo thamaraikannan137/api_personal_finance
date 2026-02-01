aef34c870a6786e225473fd77a9dd239e0facd8f
thamaraikannan137 <thamaraikannan137@gmail.com>
2026-02-01 22:35:18 +0530

    adding the aws domain

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,9 @@ const helmetInitializer: HelmetInitializer =
 app.use(helmetInitializer());
 app.use(
   cors({
-    origin: [env.CORS_ORIGIN, "http://localhost:5173","https://ui-personal-finance.vercel.app"],
+    origin: [env.CORS_ORIGIN, "http://localhost:5173","https://ui-personal-finance.vercel.app","http://ec2-3-109-214-68.ap-south-1.compute.amazonaws.com",
+      "https://ec2-3-109-214-68.ap-south-1.compute.amazonaws.com"
+    ],
     credentials: true,
   })
 );
